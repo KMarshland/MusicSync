9278c2469b3b20bd88084d57da423f076cfc5fc0
Kai <kaimarshland@gmail.com>
2014-06-19 21:22:11 -0700

    First commit

---
+++ pyitunes-master/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyItunes",
+    version="1.1",
+    author="",
+    author_email="",
+    url="https://github.com/liamks/pyitunes",
+    description="Python Itunes Library parser",
+    long_description=open("README.md").read(),
+    classifiers=[],
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=[]
+    )
